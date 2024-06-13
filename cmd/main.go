c088394506007e19a55804a87175d91ba1f06323
faruqii <faruqihafiz21@gmail.com>
2024-06-13 20:52:34 +0700

    feat/deploy

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Magetan-Boyz/Backend/internal/app"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
+	// err := godotenv.Load()
 
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println("Error loading .env file")
+	// 	return
+	// }
 
 	app.Start()
 }
