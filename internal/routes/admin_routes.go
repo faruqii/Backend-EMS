e3095ebad778fa91e2ecb2ddd2403dd35520f7da
Faruqi Hafiz <faruqihafiz21@gmail.com>
2024-06-19 03:11:15 +0700

    feat: Add import functionality to create teacher and parent accounts from CSV
    This commit adds the ability to import teacher and parent accounts from a CSV file. Two new routes, "/teacher/import" and "/parent/import", have been added to the admin routes. These routes allow administrators to bulk create teacher and parent accounts by providing a CSV file containing the necessary information. This feature enhances the application by simplifying the account creation process for administrators.

--- internal/routes/admin_routes.go
+++ internal/routes/admin_routes.go
@@ -23,6 +23,7 @@ func AdminRoutes(router fiber.Router, adminSvc services.AdminService, mw *middle
 	// Teacher routes
 	teacherRoutes := adminRoutes.Group("/teacher")
 	teacherRoutes.Post("/create", admin.CreateTeacher)
+	teacherRoutes.Post("/import", admin.CreateTeacherAccountFromCsv)
 	teacherRoutes.Get("/all", admin.GetAllTeacher)
 	teacherRoutes.Put("/:id/update-homeroom-status", admin.UpdateTeacherHomeroomStatus)
 	teacherRoutes.Get("/:id/subjects", admin.GetTeacherSubjects)
@@ -50,6 +51,7 @@ func AdminRoutes(router fiber.Router, adminSvc services.AdminService, mw *middle
 	// Parent routes
 	parentRoutes := adminRoutes.Group("/parent")
 	parentRoutes.Post("/create", admin.CreateParentAccount)
+	parentRoutes.Post("/import", admin.CreateParentAccountFromCsv)
 	parentRoutes.Post("/assign-student", admin.AssignParentToStudent)
 
 	// Announcement routes
